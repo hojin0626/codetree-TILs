2432468eadf9c7549ea85b722b88c46bf3393c26
hojin-hub <54978797+hojin0626@users.noreply.github.com>
2024-04-02 08:37:16 +0900

    Title: 100 도달하기, Time: 108ms, Memory: 34MB, Status: Passed - CodeTree

--- 240402/100 도달하기/reach-100.py
+++ 240402/100 도달하기/reach-100.py
@@ -4,8 +4,8 @@ arr = [1, n]
 i = 2
 elem = 0
 while elem < 100:
-    arr.append(arr[i - 2] + arr[i - 1])
-    elem = arr[i]
+    elem = arr[i - 2] + arr[i - 1]
+    arr.append(elem)
     i += 1
 
 for ent in arr:
