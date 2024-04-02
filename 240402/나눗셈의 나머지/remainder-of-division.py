ba348c510d8a712ff07b86bef6381cb336275bd0
hojin-hub <54978797+hojin0626@users.noreply.github.com>
2024-04-02 11:51:02 +0900

    Title: 나눗셈의 나머지, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240402/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+a, b = arr[0], arr[1]
+
+n = 10
+count_arr = [0] * n
+while a >= 1:
+    tmp = a % b
+    a //= b
+    count_arr[tmp] += 1
+
+sum_val = 0
+for i in range(n):
+    sum_val += count_arr[i] * count_arr[i]
+
+print(sum_val)
